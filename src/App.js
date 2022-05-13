e2721bb7b453091db0ae5d019f4490b1980faac8
Vasco Singh <88992223+VascoSingh@users.noreply.github.com>
2022-05-13 13:57:29 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./index.css";
+import { userState } from 'react';
+
+function App(props) {
+  //const [grid, setGrid] = userState('');
+  //setGrid(Array(9).fill('x').map(()=>Array(9).fill('x')));
+  return (
+    <div className="App">
+      <nav><h1>Plonkle</h1></nav>
+    </div>
+  );
+}
+
+
+export default App;
