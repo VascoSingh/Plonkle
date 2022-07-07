74b2904971132163302687a0ac73b32e1a50173e
Vasco Singh <vasco@realignrx.com>
2022-07-07 00:05:32 -0500

    Finalized layout and modular hand

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./index.css";
 //import {DndProvider} from 'react-dnd'
 //import {HTML5Backend} from 'react-dnd-html5-backend'
 //import { userState } from 'react';
-import Board from "./components/Board";
-import Tile from "./components/Tile";
+//import Board from "./components/Board";
+//import Tile from "./components/Tile";
 import Grid from "./components/Grid"
+import Hand from "./components/Hand";
 
 function App(props) {
   //const [grid, setGrid] = userState('');
@@ -14,81 +15,8 @@ function App(props) {
     //<DndProvider>
       <div className="App">
         <nav><h1>Plonkle</h1></nav>
-        <Grid>tesst</Grid>
-        <div className="row1flex">
-          <Board id="board-1" className="board">
-            <Tile id="tile-1" className="tile" draggable="true">
-              <p>A</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-2" className="board">
-            <Tile id="tile-2" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-3" className="board">
-            <Tile id="tile-3" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-4" className="board">
-            <Tile id="tile-4" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-5" className="board">
-            <Tile id="tile-5" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-6" className="board">
-            <Tile id="tile-6" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-        </div>
-        <div className="row2flex">
-          <Board id="board-7" className="board">
-            <Tile id="tile-7" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-8" className="board">
-            <Tile id="tile-8" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-        
-          <Board id="board-9" className="board">
-            <Tile id="tile-9" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-10" className="board">
-            <Tile id="tile-10" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-11" className="board">
-            <Tile id="tile-11" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-
-          <Board id="board-12" className="board">
-            <Tile id="tile-12" className="tile" draggable="true">
-              <p>B</p>
-            </Tile>
-          </Board>
-        </div>
+        <Grid></Grid>
+        <Hand></Hand>
       </div>
     //</DndProvider>
   );
--- src/components/Hand.js
+++ src/components/Hand.js
@@ -2,9 +2,21 @@ import React from 'react'
 import Board from "./Board";
 import Tile from './Tile';
 
+const letters = ['A', 'C', 'T', 'U', 'N', 'I', 'X', 'S', 'H', 'R', 'O', 'P'];
+let handList = [];
+for (let i = 0; i < 12; i++) {
+    handList.push(<Board id={"" + i/6 + "" + i%6} className="board">
+        <Tile id={"tile" + i} className="tile" draggable="true">
+            <p>{letters[i]}</p>
+        </Tile>
+    </Board>)
+  }
+
 function Hand() {
   return (
-    <div>Hand</div>
+    <div className="handflex">
+        {handList}
+    </div>
   )
 }
 
