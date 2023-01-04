dd33d2ea64536f0482265e9376e83f3fc5e1a4a5
Vasco Singh <vasco@realignrx.com>
2023-01-03 21:46:00 -0800

    Fixed import

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 import "./index.css";
-//import {DndProvider} from 'react-dnd'
-//import {HTML5Backend} from 'react-dnd-html5-backend'
-//import Board from "./components/Board";
-//import Tile from "./components/Tile";
 import Grid from "./components/Grid"
 import Hand from "./components/Hand";
-import Board from "./components/Board";
+
 
 
 function App(props) {
-  //const [grid, setGrid] = useState(Array(9).fill(' ').map(()=>Array(9).fill(' ')));
-  //grid[1][1] = 'x';
-  //setGrid(grid);
-  //Array(9).fill('x').map(()=>Array(9).fill('x'))
   return (
     <div className="App">
       <nav><h1>Plonkle</h1></nav>
